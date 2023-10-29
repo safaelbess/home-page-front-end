3936285436c3d5e83370e9c4e2b8526e57fce55e
safaelbess <136888825+safaelbess@users.noreply.github.com>
2023-10-29 14:29:06 -0700

    fun

--- pages/js/script.js
+++ pages/js/script.js
@@ -5,3 +5,26 @@ function myFunction(){
     document.getElementById("demo").innerHTML="paragraph changed";
     console.log('hi');
 }
+
+
+function sumFunction(num1,num2)
+{
+    let sum= num1+num2;
+    console.log(sum);
+    return(sum);
+}
+sumFunction(20,40);
+console.log(sumFunction(30,10));
+
+
+var body=document.getElementsByTagName('body');
+console.log(body[0].style.backgroundColor);
+function changeBackground()
+{body[0].style.backgroundColor=='pink';
+if (body[0].style.backgroundColor=='pink')
+     body[0].style.backgroundColor='red';
+     else
+     body[0].style.backgroundColor='pink';
+
+}
+
