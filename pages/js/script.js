9772362bf821b45b9f483dc629d6f1220eb8cf20
safaelbess <136888825+safaelbess@users.noreply.github.com>
2023-11-01 00:15:02 -0700

    event

--- pages/js/script.js
+++ pages/js/script.js
@@ -18,17 +18,40 @@ console.log(sumFunction(30,10));
 
 
 var body=document.getElementsByTagName('body');
-var footer=document.getElementsById("footerDiv");
+var footer=document.getElementsById("footerDiv")[0];
+var header= document.getElementById('headerDiv')[0];
 
 console.log(body[0].backgroundColor);
 function changeBackground()
 {
 
 if (body[0].style.backgroundColor=='whitesmoke')
-     body[0].style.backgroundColor='red';
-    // document.getElementsById("footerDiv").style.backgroundColor='grey';}
-else
-    body[0].style.backgroundColor='whitesmoke';
-     //document.getElementsById("footerDiv").style.backgroundColor='black';}
+    { body[0].style.backgroundColor='red';
+    footer.style.backgroundColor='grey';
+   header.style.backgroundColor='grey';
 
+}
+else
+   { body[0].style.backgroundColor='whitesmoke';
+     document.getElementsById("footerDiv").style.backgroundColor='black';
+     header.style.backgroundColor='black';
+
+    }
+}
+
+var myForm= document.getElementsByClassName('userinput')[0];
+var myInput=Document.getElementsByClassName("input")[0];
+var myName =Document.getElementsByClassName("myName")[0];
+
+myForm.addEventListener( 'submit',function(e)
+{e.preventDefault()
+    console.log(e);
+    myName.innerHTML=myInput.value
+    myInput.value=""
+    myName.style.backgroundColor="red"
+    myName.style.fontWeight="bold"
+    myName.style.fontFamily="arial"
+    myName.style.textAligh="center"
+
+})
 
